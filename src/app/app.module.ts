5653cd435dfd2c66a982db0c2fb3885b40d2f09a
CarTig <carolle.t.n@gmail.com>
2024-04-25 12:50:16 +0200

    image test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageComponentComponent } from './image-component/image-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageComponentComponent
   ],
   imports: [
     BrowserModule,
